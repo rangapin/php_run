f8a1de2225b512fd618a77663486e8a48f9455af
rangapin <rangapin@gmx.com>
2023-06-09 01:06:09 +0200

    tailwind configuration

--- action.php
+++ action.php
@@ -3,6 +3,6 @@
 $amount = $_POST['amount'];
 $result = $amount * 0.03;
 
-if (isset($amount)) {
+if(isset($amount)) {
     echo "This is equal " . $result . " EUR";
   }
--- index.php
+++ index.php
@@ -6,11 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Currency exchange</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+          }
+        }
+      }
+    }
+    </script>
+
+    <style type="text/tailwindcss">
+    @layer utilities {
+      .content-auto {
+        content-visibility: auto;
+      }
+    }
+    </style>
 </head>
 
 <body>
     <form action="action.php" method="post">
-    <p>Please enter the amount in EGP: <input type="number" name="amount" /></p>
+    <p class="text-red-500">Please enter the amount in EGP: <input type="number" name="amount" /></p>
     <p><input type="submit" /></p>
 </body>
 
