33894289d90cad98ce1284c5aba61676acf37305
rangapin <rangapin@gmx.com>
2023-06-13 00:56:08 +0200

    added back button

--- action.php
+++ action.php
@@ -1,3 +1,5 @@
+<?php require 'header.php' ?>
+
 <?php 
 
 $amount = $_POST['amount'];
@@ -6,3 +8,6 @@ $result = $amount * 0.03;
 if(isset($amount)) {
     echo "This is equal " . $result . " EUR";
   }
+?>
+<br>
+<a href="index.php" class="w-30 bg-teal-600 mx-3 mt-3 rounded">Back</a>
--- index.php
+++ index.php
@@ -1,37 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Currency exchange</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-          }
-        }
-      }
-    }
-    </script>
-
-    <style type="text/tailwindcss">
-    @layer utilities {
-      .content-auto {
-        content-visibility: auto;
-      }
-    }
-    </style>
-</head>
+<?php require 'header.php' ?>
 
 <body>
+
     <form action="action.php" method="post">
-    <p class="text-red-500">Please enter the amount in EGP: <input type="number" name="amount" /></p>
+    <p class="text-black-500">Please enter the amount in EGP: <input type="number" name="amount" /></p>
     <p><input type="submit" /></p>
+    
 </body>
 
 </html>
