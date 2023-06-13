87b870218dcb3428754a53b55d7c2ef72f60dd95
rangapin <rangapin@gmx.com>
2023-06-14 00:37:40 +0200

    messing with tailwind

--- action.php
+++ action.php
@@ -1,13 +1,13 @@
-<?php require 'header.php' ?>
-
-<?php 
+<?php require 'header.php';
 
 $amount = $_POST['amount'];
 $result = $amount * 0.03;
 
 if(isset($amount)) {
+    echo '<div class="flex flex-wrap h-72 w-100 bg-gray-100 justify-around items-center"</div>';
     echo "This is equal " . $result . " EUR";
+    echo "</div>";
   }
 ?>
 <br>
-<a href="index.php" class="w-30 bg-teal-600 mx-3 mt-3 rounded">Back</a>
+<a href="index.php" class="w-40 h-20 bg-slate-600 mx-5 mt-5 my-5 p-1 rounded">Back</a>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php require 'header.php' ?>
 
 <body>
-
+  <div class="flex flex-wrap h-72 w-100 bg-gray-100 justify-around items-center">
     <form action="action.php" method="post">
-    <p class="text-black-500">Please enter the amount in EGP: <input type="number" name="amount" /></p>
-    <p><input type="submit" /></p>
-    
+    <p>Please enter the amount in EGP: <input type="number" name="amount" /></p>
+    <p class="justify-center"><input class="w-20 h-7 bg-teal-600 mx-4 mt-4 rounded" type="submit" /></p>
+  </div>
 </body>
 
 </html>
+
